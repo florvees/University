67f1de85887cb022d91e54a7e27a974461737d04
florvees <daniilfigurin2@gmail.com>
2024-02-26 20:05:02 +0200

    Update 2_6Homework.cpp

--- C++/2nd semester/6. Vector bool/2_6Homework.cpp
+++ C++/2nd semester/6. Vector bool/2_6Homework.cpp
@@ -1,6 +1,44 @@
 ﻿#include <iostream>
 
-int main()
+namespace df
 {
-    std::cout << "Test" << std::endl;
+    template<typename T>
+    class vector
+    {
+    private:
+    public:
+    };
+
+    template<>
+    class vector<bool>
+    {
+    private:
+        char* arr;
+        size_t byte_lenght;
+        size_t bit_lenght;
+    public:
+        vector<bool>(size_t lenght)
+        {
+            byte_lenght = lenght / 8;
+            bit_lenght = lenght % 8;
+            arr = new char[byte_lenght];
+        }
+        ~vector<bool>()
+        {
+            delete[] arr;
+        }
+        size_t byte_getter()
+        {
+            return byte_lenght;
+        }
+        size_t bit_getter()
+        {
+            return bit_lenght;
+        }
+    };
+}
+
+int main()  
+{
+
 }
