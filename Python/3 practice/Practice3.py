9af5029c91ae47e96bb5b95f0f36ba66ae7ee481
florvees <daniilfigurin2@gmail.com>
2024-03-26 20:56:41 +0200

    ex2 version 2

--- Python/3 practice/Practice3.py
+++ Python/3 practice/Practice3.py
@@ -23,20 +23,15 @@
 
 line = str(input("Enter your shorted line: "))
 new_line = ""
-flag = True
-for i in range(len(line)-1):
-    if i < len(line) - 1:
-        if(line[i+1].isdigit()):
-            amount = int(line[i+1])
-            if flag:
-                for j in range(amount):
-                    new_line+=line[i]
-                flag = False
-            else:
-                for j in range(amount-1):
-                    new_line+=line[i]
-        else:
-            new_line+=line[i+1]
-    else:
-        new_line+=line[i+1]
+i = 0
+while i < len(line):
+	if i < len(line) - 1:
+		if line[i + 1].isdigit():
+			new_line += line[i] * int(line[i + 1])
+			i+= 1
+		else:
+			new_line += line[i]
+	else:
+		new_line += line[i]
+	i += 1
 print("Here's the line: " + new_line)
