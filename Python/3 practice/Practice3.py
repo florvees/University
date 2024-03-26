9cd5fd6ba1fe031f2bc1d3683e47e551463ec2d3
florvees <daniilfigurin2@gmail.com>
2024-03-26 20:12:58 +0200

    1st ex

--- Python/3 practice/Practice3.py
+++ Python/3 practice/Practice3.py
@@ -1,1 +1,19 @@
-print("Hello, wolrd!")
+# Ex 1
+line = input("Enter your string: ")
+new_line = ""
+i = 0
+while i < len(line):
+	if i == len(line) - 1:
+		new_line += line[i]
+		break
+	counter = 1
+	while (line[i] == line[i+counter]):
+		counter += 1
+		if (i+counter >= len(line)):
+			break
+	new_line += line[i]
+	if counter > 1:
+		new_line += str(counter)
+		i += counter-1
+	i += 1
+print("Here's the shorted version: ",new_line)
