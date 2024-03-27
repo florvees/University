3fc28771fb8e1f37ef13bd4f3ce6ef4a7bd22b96
florvees <daniilfigurin2@gmail.com>
2024-03-27 02:06:59 +0200

    3rd practice finished, 4th is almost done

--- Python/4 practice/Practice4.py
+++ Python/4 practice/Practice4.py
@@ -1,1 +1,26 @@
-print("Hello, wolrd!")
+# Ex 1
+
+#print("There is "+str(len(set(str(input("Enter your numbers: ")).split())))+" unique number(s) in the set")
+
+# Ex 2
+
+# first_set = sorted(set(str(input("Enter your first set: ")).split()))
+# second_set = sorted(set(str(input("Enter your second set: ")).split()))
+# if (first_set[:len(second_set)] == second_set):
+#     print("True")
+# else:
+#     print("False")
+
+# Ex 3
+
+n = int(input("How many cities?"))
+cities = set()
+for i in range(n):
+    city = input("Please, enter the city: ").strip().lower()
+    if city in cities:
+        print("REPEAT")
+    else:
+        cities.add(city)
+        print("OK")
+
+        
