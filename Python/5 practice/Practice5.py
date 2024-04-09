e2a01119d7ed5d244a143895c59eaf2ea3d106d9
florvees <daniilfigurin2@gmail.com>
2024-04-10 00:32:46 +0200

    1st and 2nd

--- Python/5 practice/Practice5.py
+++ Python/5 practice/Practice5.py
@@ -1,5 +1,18 @@
 # Ex 1
 
-with open('input1.txt', 'r') as file:
-    data = file.read()
-    print(data)
+# prod = 1
+# with open("input1.txt", "r", encoding = "utf-8") as file_in:
+#     data = file_in.readline().split()
+#     for elem in data:
+#         prod *= int(elem)
+# with open("output1.txt", "w", encodin = "utf-8") as file_out:
+#     file_out.write(prod)
+
+# Ex 2
+
+data = []
+with open("input2.txt", "r", encoding = "utf-8") as file_in:
+    data = file_in.read().splitlines()
+    data.sort()
+with open("output2.txt", "w", encoding = "utf-8") as file_out:
+    file_out.write('\n'.join(data))
