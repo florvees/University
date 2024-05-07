74f3c13865c654a50ea95a3157e0c31faf03f4be
florvees <daniilfigurin2@gmail.com>
2024-05-07 22:24:31 +0200

    3rd's done

--- Python/5 practice/Practice5.py
+++ Python/5 practice/Practice5.py
@@ -1,16 +1,15 @@
 # Ex 1
 
-prod = 1
-with open("1st/input1.txt", "r", encoding="utf-8") as file_in:
-    data = file_in.readline().split()
-    for elem in data:
-        prod *= int(elem)
-with open("1st/output1.txt", "w", encoding="utf-8") as file_out:
-    file_out.write(str(prod))
+# prod = 1
+# with open("1st/input1.txt", "r", encoding="utf-8") as file_in:
+#     data = file_in.readline().split()
+#     for elem in data:
+#         prod *= int(elem)
+# with open("1st/output1.txt", "w", encoding="utf-8") as file_out:
+#     file_out.write(str(prod))
 
 # Ex 2
 
-# data = []
 # with open("2nd/input2.txt", "r", encoding="utf-8") as file_in:
 #     data = file_in.read().splitlines()
 #     data.sort()
@@ -19,4 +18,14 @@ with open("1st/output1.txt", "w", encoding="utf-8") as file_out:
 
 # Ex 3
 
+# data = []
+# with open("3rd/input3.txt", "r", encoding="utf-8") as file_in:
+#     for line in file_in.read().splitlines():
+#         data.append(line.split())
+#     data.sort(key=lambda x: int(x[2]))
+# with open("3rd/output3_younger.txt", "w", encoding="utf-8") as file_younger:
+#     file_younger.write(data[0][0]+" "+data[0][1]+" "+str(data[0][2]))
+# with open("3rd/output3_older.txt", "w", encoding="utf-8") as file_older:
+#     file_older.write(data[-1][0]+" "+data[-1][1]+" "+str(data[-1][2]))
 
+# Ex 4
