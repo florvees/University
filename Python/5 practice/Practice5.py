25d26d4d89905c913929bbbb3d52ec3e5f053cc4
florvees <daniilfigurin2@gmail.com>
2024-05-07 21:56:27 +0200

    work in progress

--- Python/5 practice/Practice5.py
+++ Python/5 practice/Practice5.py
@@ -1,18 +1,22 @@
 # Ex 1
 
-# prod = 1
-# with open("input1.txt", "r", encoding = "utf-8") as file_in:
-#     data = file_in.readline().split()
-#     for elem in data:
-#         prod *= int(elem)
-# with open("output1.txt", "w", encodin = "utf-8") as file_out:
-#     file_out.write(prod)
+prod = 1
+with open("1st/input1.txt", "r", encoding="utf-8") as file_in:
+    data = file_in.readline().split()
+    for elem in data:
+        prod *= int(elem)
+with open("1st/output1.txt", "w", encoding="utf-8") as file_out:
+    file_out.write(str(prod))
 
 # Ex 2
 
-data = []
-with open("input2.txt", "r", encoding = "utf-8") as file_in:
-    data = file_in.read().splitlines()
-    data.sort()
-with open("output2.txt", "w", encoding = "utf-8") as file_out:
-    file_out.write('\n'.join(data))
+# data = []
+# with open("2nd/input2.txt", "r", encoding="utf-8") as file_in:
+#     data = file_in.read().splitlines()
+#     data.sort()
+# with open("2nd/output2.txt", "w", encoding="utf-8") as file_out:
+#     file_out.write('\n'.join(data))
+
+# Ex 3
+
+
