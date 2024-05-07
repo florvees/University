c94b6367d98438c36ee2212e93abc608becb384d
florvees <daniilfigurin2@gmail.com>
2024-05-07 23:37:31 +0200

    qwerty123

--- Python/2 practice/Practice2.py
+++ Python/2 practice/Practice2.py
@@ -1,36 +1,17 @@
-#Practice 1, 1 ex.
+# Ex 1
 
-first, second, third = int(input()), int(input()), int(input())
-if (first==second==third):
-    print(first, second, third)
-elif (first==second!=third):
-    print(first, second)
-elif (first==third!=second):
-    print(first, third)
-elif (second==third!=first):
-    print(second, third)
-else:
-    print("No equal numbers")
+n = int(input())
 
-#Practice 1, 2.1 ex
+prev_str = [1]
+for i in range(n):
+    print(" "*(n-i), end='')
+    print(*prev_str)
+    current_str = prev_str.copy()
+    current_str.append(1)
+    for j in range(1, len(current_str) - 1):
+        current_str[j] = prev_str[j - 1] + prev_str[j]
+    prev_str = current_str
 
-# n = int(input())
-# endl = ' '*((n+1)//10)
-# for i in range(n+1):
-#     for j in range(i):
-#         print(j+1,end=endl)
-#     print('\t')
+# Ex 2
 
-#Practice 1, 2.2 ex
-    
-# n = int(input())
-# free_space = '  '*(n//10)
-# free_space_test = '  '*((n//10)+1)
-
-# for i in range(n):
-#     print(' '*(n-i-1),end=' ')
-#     for j in range(i+1):
-#         print(i-j+1,end=free_space)
-#     for j in range(i):
-#         print(j+2,end=free_space)
-#     print(' '*(n-i-1),end='\n')
+# хз
