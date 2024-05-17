917e2350be325466113e1cd2d6aba9cf5cd559fb
florvees <144287244+florvees@users.noreply.github.com>
2024-05-17 22:38:34 +0200

    4th is almost done(has some bugs tho)

--- C++/2nd semester/4. Long arithmetic/4. Long arithmetic.cpp
+++ C++/2nd semester/4. Long arithmetic/4. Long arithmetic.cpp
@@ -47,8 +47,6 @@ namespace df
             this->is_negative = other.is_negative;
             this->data = new int[data_size];
 
-            //memcpy(this->data, other.data, data_size * sizeof(int));
-
             for (int i = 0; i < this->data_size; i++)
             {
                 this->data[i] = other.data[i];
@@ -59,7 +57,22 @@ namespace df
             delete[] this->data;
             this->data = nullptr;
         }
-        bool operator<(BigInt& other) 
+        BigInt& operator=(const BigInt& other)
+        {
+            delete[] this->data;
+
+            this->data_size = other.data_size;
+            this->is_negative = other.is_negative;
+            this->data = new int[data_size];
+
+            for (int i = 0; i < this->data_size; i++)
+            {
+                this->data[i] = other.data[i];
+            }
+
+            return *this;
+        }
+        bool operator<(BigInt& other)
         {
             if (this->is_negative != other.is_negative)
                 return this->is_negative;
@@ -89,6 +102,7 @@ namespace df
             }
             return false;
         }
+
         bool operator>(BigInt& other)
         {
             return other < *this;
@@ -101,15 +115,126 @@ namespace df
         {
             return !(*this == other);
         }
+        BigInt operator+(BigInt& other)
+        {
+            BigInt result;
+            if (this->is_negative == other.is_negative) {
+                result.data_size = std::max(data_size, other.data_size) + 1;
+                result.data = new int[result.data_size];
+                int carry = 0;
+                for (int i = 0; i < result.data_size; ++i) {
+                    int sum = carry;
+                    if (i < data_size)
+                        sum += this->data[i];
+                    if (i < other.data_size)
+                        sum += other.data[i];
+                    result.data[i] = sum % 10;
+                    carry = sum / 10;
+                }
+                result.is_negative = this->is_negative;
+                if (result.data[result.data_size - 1] == 0)
+                    --result.data_size;
+            }
+            else {
+                BigInt copy = *this;
+                result.data_size = std::max(data_size, other.data_size);
+                result.data = new int[result.data_size];
+                bool flag = false;
+                if (copy.data_size == other.data_size) {
+                    for (int i = 0; i < copy.data_size; i++) {
+                        if (copy.data[i] > other.data[i]) {
+                            flag = true;
+                        }
+                    }
+                }
+                if (copy.data_size > other.data_size)
+                    flag = true;
+                if (flag)
+                {
+                    for (int i = 0; i < result.data_size; i++)
+                    {
+                        if (copy.data[i] < 0)
+                        {
+                            copy.data[i] += 10;
+                            copy.data[i + 1] -= 1;
+                        }
+                        if (i >= other.data_size)
+                        {
+                            result.data[i] = copy.data[i];
+                            continue;
+                        }
+                        if (copy.data[i] - other.data[i] < 0)
+                        {
+                            copy.data[i] += 10;
+                            copy.data[i + 1] -= 1;
+                        }
+                        result.data[i] = copy.data[i] - other.data[i];
+                    }
+                    result.is_negative = (copy.is_negative > other.is_negative);
+                }
+                else {
+                    for (int i = 0; i < result.data_size; i++)
+                    {
+                        if (other.data[i] < 0)
+                        {
+                            other.data[i] += 10;
+                            other.data[i + 1] -= 1;
+                        }
+                        if (i >= copy.data_size)
+                        {
+                            result.data[i] = other.data[i];
+                            continue;
+                        }
+                        if (other.data[i] - copy.data[i] < 0)
+                        {
+                            other.data[i] += 10;
+                            other.data[i + 1] -= 1;
+                        }
+                        result.data[i] = other.data[i] - copy.data[i];
+                    }
+                    result.is_negative = (other.is_negative > copy.is_negative);
+                    if (result.data[result.data_size - 1] == 0)
+                        --result.data_size;
+                }
+            }
+            return result;
+        }
+
+        friend std::ostream& operator<<(std::ostream& out, BigInt& other);
+        friend std::istream& operator>>(std::istream& in, BigInt& other);
     };
+
+    std::ostream& operator<<(std::ostream& out, BigInt& other)
+    {
+        if (other.is_negative)
+        {
+            out << '-';
+        }
+        for (int i = 0; i < other.data_size; i++)
+        {
+            out << other.data[i];
+        }
+        return out;
+    }
+
+    std::istream& operator>>(std::istream& in, BigInt& other)
+    {
+        std::string value;
+        in >> value;
+        other = BigInt(value);
+        return in;
+    }
+
 }
 
 int main()
 {
-    df::BigInt test("12145");
-    df::BigInt cerf("12415");
-    if (test < cerf)
-        std::cout << "Yes" << std::endl;
-    else
-        std::cout << "No" << std::endl;
+    df::BigInt test("15");
+    df::BigInt cerf("-18");
+    df::BigInt goida(test + cerf);
+    std::cout << test << std::endl;
+    std::cout << cerf << std::endl;
+    std::cout << goida << std::endl;
+    test = cerf;
+    std::cout << cerf << std::endl;
 }
