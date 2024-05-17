63a3e55022e75c4e8ff9d30e6b7bb5affcfc9049
florvees <144287244+florvees@users.noreply.github.com>
2024-05-17 20:46:50 +0200

    logic operators are done

--- C++/2nd semester/4. Long arithmetic/4. Long arithmetic.cpp
+++ C++/2nd semester/4. Long arithmetic/4. Long arithmetic.cpp
@@ -5,10 +5,11 @@ namespace df
 {
     class BigInt 
     {
-    public:
+    private:
         int* data;
         int data_size;
         bool is_negative;
+    public:
         BigInt() 
         {
             this->data = nullptr;
@@ -46,6 +47,8 @@ namespace df
             this->is_negative = other.is_negative;
             this->data = new int[data_size];
 
+            //memcpy(this->data, other.data, data_size * sizeof(int));
+
             for (int i = 0; i < this->data_size; i++)
             {
                 this->data[i] = other.data[i];
@@ -56,29 +59,57 @@ namespace df
             delete[] this->data;
             this->data = nullptr;
         }
+        bool operator<(BigInt& other) 
+        {
+            if (this->is_negative != other.is_negative)
+                return this->is_negative;
+            if (this->is_negative)
+            {
+                if (this->data_size > other.data_size)
+                    return true;
+                for (int i = 0; i < data_size; i++)
+                {
+                    if (this->data[i] > other.data[i])
+                    {
+                        return true;
+                    }
+                }
+            }
+            else
+            {
+                if (this->data_size < other.data_size)
+                    return true;
+                for (int i = 0; i < data_size; i++)
+                {
+                    if (this->data[i] < other.data[i])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        bool operator>(BigInt& other)
+        {
+            return other < *this;
+        }
+        bool operator==(BigInt& other)
+        {
+            return !(*this < other) && !(other < *this);
+        }
+        bool operator!=(BigInt& other)
+        {
+            return !(*this == other);
+        }
     };
 }
 
 int main()
 {
-    df::BigInt test("-12345");
-    std::cout << test.is_negative << std::endl;
-    std::cout << test.data_size << std::endl;
-    for (int i = 0; i < test.data_size; i++)
-    {
-        std::cout << test.data[i];
-    }
-    std::cout << "\nggggg" << std::endl;
-
-    df::BigInt test2("12415");
-    for (int i = 0; i < test2.data_size; i++)
-    {
-        std::cout << test2.data[i];
-    }
-    test2 = test;
-    std::cout << "\nNew data" << std::endl;
-    for (int i = 0; i < test2.data_size; i++)
-    {
-        std::cout << test2.data[i];
-    }
+    df::BigInt test("12145");
+    df::BigInt cerf("12415");
+    if (test < cerf)
+        std::cout << "Yes" << std::endl;
+    else
+        std::cout << "No" << std::endl;
 }
